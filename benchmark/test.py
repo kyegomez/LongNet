1dfbbd16331676e5765b027354d947d389b2a4c5
Kye <kye@apacmediasolutions.com>
2023-07-06 07:46:56 -0400

    benchmakrs

---
+++ benchmark/test.py
@@ -0,0 +1,36 @@
+import timeit
+import torch
+from LongNet import DilatedAttention
+
+
+#model config
+d_model = 512
+num_heads = 8
+dilation_rate = 2
+segment_size = 64
+
+
+#input data
+batch_size = 32
+seq_len = 1024
+
+
+#create model and data
+model = DilatedAttention(d_model, num_heads, dilation_rate, segment_size).to(device)
+x = torch.randn((batch_size, seq_len, d_model), device=device, dtype=dtype)
+
+
+#test forward pass
+with torch.no_grad():
+    output = model(x)
+    print(f"Output shape: {output.shape}") # expected (batch_size, seq_Len)
+
+
+#benchmark model
+num_runs = 1000
+start_time = timeit.default_timer()
+for _ in range(num_runs):
+    model(x)
+
+elapsed_time = timeit.default_timer() - start_time
+print(f"Average forward pass time: {elapsed_time / num_runs:.6f} seconds")
