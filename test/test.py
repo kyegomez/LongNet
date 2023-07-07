829b43093e08d29ef0be758a1b46e55a346d613e
Kye <kye@apacmediasolutions.com>
2023-07-07 13:26:42 -0400

    clean up testing script

--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 import torch
 import unittest
 from transformers import TrainingArguments, Trainer
-from LongNet import LongNetSelector, LongNetTokenizer
+from LongNet.model import LongNetTokenizer, LongNet, 
+
 class TestLongNetModels(unittest.TestCase):
     def setUp(self):
-        self.longnet_selector = LongNetSelector()
+        self.model = LongNet()
         self.tokenizer = LongNetTokenizer()
         self.training_args = TrainingArguments(
             output_dir="./test_output",
@@ -16,18 +17,8 @@ class TestLongNetModels(unittest.TestCase):
             logging_dir='./test_logs',
         )
 
-    def test_multimodal_model(self):
-        model = self.longnet_selector.get_model(LongNetType.MULTIMODAL)
-        train_dataset = self.get_sample_dataset()
-        trainer = Trainer(
-            model=model,
-            args=self.training_args,
-            train_dataset=train_dataset,
-        )
-        trainer.train()
-
     def test_language_model(self):
-        model = self.longnet_selector.get_model(LongNetType.LANGUAGE_ONLY)
+        model = self.model
         train_dataset = self.get_sample_dataset()
         trainer = Trainer(
             model=model,
@@ -38,10 +29,7 @@ class TestLongNetModels(unittest.TestCase):
 
     def get_sample_dataset(self):
         # generate a simple dataset for testing
-        data = {
-            'target_text': ["This is a test sentence."] * 10,
-            'image': torch.rand(10, 3, 224, 224) # 10 random images
-        }
+        data = {'target_text': ["This is a test sentence."] * 10,}
 
         # Tokenize dataset
         inputs = self.tokenizer.tokenize(data)
