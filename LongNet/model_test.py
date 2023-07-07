ed730c1c0a7166803d24b1cb55057bcbc71a0c31
Kye <kye@apacmediasolutions.com>
2023-07-07 08:08:47 -0400

    longnet test

--- LongNet/model_test.py
+++ LongNet/model_test.py
@@ -1,28 +1,34 @@
 # from LongNet.model import LongNetTokenizer, LongNetSelector
-
+import torch
 from model import LongNetTokenizer, LongNetSelector
 
-longnet_selector = LongNetSelector()
-tokenizer = LongNetTokenizer()
+class LongNetTest:
+    def __init__(self):
+        self.longnet_selector = LongNetSelector()
+        self.tokenizer = LongNetTokenizer()
 
+    def run_test(self, model_type: str):
+        data = {
+            'target_text': ["This is a test sentence."] * 2,
+            'image': torch.rand(2, 3, 224, 224) # 2 random images
+        }
 
-data = {
-    "target_text": ["This is a test sequence"] * 2,
-    # 'image': torch.rand(2, 3, 224, 224) # 2 random images
-}
+        inputs = self.tokenizer.tokenize(data)
+        
+        if model_type.lower() == 'multimodal':
+            self._test_model('multimodal', inputs)
+        elif model_type.lower() == 'language':
+            self._test_model('language', inputs)
+        else:
+            raise ValueError(f"Invalid model_type: {model_type}. Please use either 'multimodal' or 'language'.")
 
-#tokenize the inputs
-inputs = tokenizer.tokenize(data)
+    def _test_model(self, model_type: str, inputs: dict):
+        print(f"Testing {model_type} LongNet model...")
+        model = self.longnet_selector.get_model(model_type)
+        outputs = model(**inputs)
+        print(f"{model_type} LongNet model forward pass succeeded!")
 
-# #test multo-modal model
-# print(f"TTesting multi-modal longnet modal")
-# model = longnet_selector.get_model("multimodal")
-# outputs = model(**inputs)
-
-#test language only longnet model
-print("Testing language only model")
-model = longnet_selector.get_model("language")
-outputs = model(**inputs)
-
-
-print("Langauge only longnet forward pass succeeed!")
+# # Now you can use the class like this:
+# tester = LongNetTest()
+# tester.run_test('multimodal')
+# tester.run_test('language')
