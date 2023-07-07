0f0a2e7eadf8feed877ffb19225a822c8a1842f9
Kye <kye@apacmediasolutions.com>
2023-07-07 13:25:15 -0400

    init

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -2,4 +2,4 @@ from LongNet.attention import DilatedAttention
 # from LongNet.training import Train
 # from LongNet.model_test import LongNetTest
 from LongNet.torchscale.torchscale.architecture.decoder import DecoderConfig, Decoder
-from LongNet.model import LongNet, LongNetLanguage, LongNetSelector
+from LongNet.model import LongNetTokenizer, LongNet
