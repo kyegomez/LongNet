96ceff1c80e70d1f1fde113578cffe340195ea77
Kye <kye@apacmediasolutions.com>
2023-07-07 08:57:52 -0400

    casual

--- LongNet/attention.py
+++ LongNet/attention.py
@@ -8,7 +8,7 @@ device = "cuda:0"
 dtype=torch.float16
 
 class DilatedAttention(nn.Module):
-    def __init__(self, d_model, num_heads, dilation_rate, segment_size, dropout=0.0):
+    def __init__(self, d_model, num_heads, dilation_rate, segment_size, dropout=0.0, casual=False):
         super(DilatedAttention, self).__init__()
         self.d_model = d_model
         self.num_heads = num_heads
@@ -16,6 +16,11 @@ class DilatedAttention(nn.Module):
         self.segment_size = segment_size
         self.attention = FlashMHA(embed_dim=d_model, num_heads=num_heads, device=device, dtype=dtype)
         self.dropout = nn.Dropout(dropout)
+
+        self.casual = casual
+
+    def get_mask(self, i, j):
+        return torch.ones((i, j), deice=device, dtype=torch.bool).triu(j - i + 2)
 
     def forward(self, x):
         batch_size, seq_len, _ = x.shape
@@ -26,6 +31,11 @@ class DilatedAttention(nn.Module):
 
         # Perform attention
         attn_output, _ = self.attention(x, x, x)
+
+        #if casual create a mask and apply to the output
+        if self.casual:
+            mask = self.get_mask(attn_output.size(1), attn_output.size(1))
+            attn_output = attn_output.masked_fill(mask, float('-inf'))
 
         #apply dropout
         attn_output = self.dropout(attn_output)
