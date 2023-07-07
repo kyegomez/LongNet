dddd72af5c29830556b8018892dcce8142a83a39
Kye <kye@apacmediasolutions.com>
2023-07-07 14:58:45 -0400

    only dilaeted attention in the __init

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -2,5 +2,5 @@
 # from LongNet.model_test import LongNetTest
 # from LongNet.torchscale.torchscale.architecture.decoder import DecoderConfig, Decoder
 # from LongNet.model import LongNetTokenizer, LongNet
-from LongNet.torchscale import RelativePositionBias, XPOS
+# from LongNet.torchscale import RelativePositionBias, XPOS
 from LongNet.attention import DilatedAttention
