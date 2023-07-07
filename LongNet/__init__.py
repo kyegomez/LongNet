f4288c4f5d000e00fa52407309b4a14b0e888b09
Kye <kye@apacmediasolutions.com>
2023-07-07 13:58:51 -0400

    clean up no complex stuff

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -1,5 +1,5 @@
 # from LongNet.training import Train
 # from LongNet.model_test import LongNetTest
-from LongNet.torchscale.torchscale.architecture.decoder import DecoderConfig, Decoder
-from LongNet.model import LongNetTokenizer, LongNet
+# from LongNet.torchscale.torchscale.architecture.decoder import DecoderConfig, Decoder
+# from LongNet.model import LongNetTokenizer, LongNet
 from LongNet.attention import DilatedAttention
