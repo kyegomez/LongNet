04dabfefe4da0c93ab70c77246c9ba2078c2c67a
Kye <kye@apacmediasolutions.com>
2023-07-07 13:43:32 -0400

    file path errors pip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'LongNet',
   packages = find_packages(exclude=[]),
-  version = '0.0.7',
+  version = '0.0.8',
   license='MIT',
   description = 'LongNet - Pytorch',
   author = 'Kye Gomez',
