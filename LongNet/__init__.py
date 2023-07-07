eaa5d90017ce41d90903a413a51da8e0f741080e
Kye <kye@apacmediasolutions.com>
2023-07-07 08:09:27 -0400

    clean up

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -1,3 +1,4 @@
 from LongNet.attention import DilatedAttention
 from LongNet.model import LongNet, LongNetLanguage, LongNetSelector, Decoder, DecoderConfig
 from LongNet.training import Train
+from LongNet.model_test import LongNetTest
