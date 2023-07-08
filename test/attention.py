8566ba1013994e6a436bc0ce58e07ed1738c4ece
Kye <kye@apacmediasolutions.com>
2023-07-07 22:51:11 -0400

    clean up

--- test/attention.py
+++ test/attention.py
@@ -1,72 +1,69 @@
-import unittest 
-import torch 
-
+import unittest
+import torch
 
 from LongNet.attention import DilatedAttention, MultiModalDilationAttention
-
 
 
 class TestDilatedAttention(unittest.TestCase):
 
     def test_output_shape(self):
-        #setup
-        input_tensor = torch.randn(2, 128, 518)
+        # Setup
+        input_tensor = torch.randn(2, 128, 512)
         dilated_attention = DilatedAttention(512, 8, 2, 64)
 
-        #action 
+        # Action
         output = dilated_attention(input_tensor)
 
-        #assert
+        # Assert
         self.assertEqual(output.shape, (2, 128, 512))
 
     def test_xpos(self):
-        #setup
+        # Setup
         input_tensor = torch.randn(2, 128, 512)
-
         dilated_attention = DilatedAttention(512, 8, 2, 64, use_xpos=True)
 
-        #action 
+        # Action
         output = dilated_attention(input_tensor)
 
-        #assert 
+        # Assert
         self.assertEqual(output.shape, (2, 128, 512))
 
     def test_relative_position_bias(self):
-        #setup
+        # Setup
         input_tensor = torch.randn(2, 128, 512)
         dilated_attention = DilatedAttention(512, 8, 2, 64, use_rel_pos_bias=True)
 
-        #action
+        # Action
         output = dilated_attention(input_tensor)
 
-        #assetr
+        # Assert
         self.assertEqual(output.shape, (2, 128, 512))
 
 
-class TestMultiModalDilatedAttention(unittest.TestCase):
+class TestMultiModalDilationAttention(unittest.TestCase):
+
     def test_output_shape(self):
-        #setup
+        # Setup
         input_tensor = [torch.randn(2, 128, 512), torch.randn(2, 128, 512)]
         multi_modal_attention = MultiModalDilationAttention(512, 8, 2, 64, num_modalities=2)
 
-        #action 
+        # Action
         output = multi_modal_attention(input_tensor)
 
-        #assert
+        # Assert
         self.assertEqual(output.shape, (2, 128, 512))
 
-    def test_single_modal(self):
-        #setup
+    def test_single_modality(self):
+        # Setup
         input_tensor = [torch.randn(2, 128, 512)]
         multi_modal_attention = MultiModalDilationAttention(512, 8, 2, 64, num_modalities=1)
 
-
-        #action
+        # Action
         output = multi_modal_attention(input_tensor)
 
-        #assert
+        # Assert
         self.assertEqual(output.shape, (2, 128, 512))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
