bfd2306b6ba94e101b2c5d7c47e23047d08b89b6
Kye <kye@apacmediasolutions.com>
2023-07-09 14:25:11 -0400

    2nd iteration

--- LongNet/attention.py
+++ LongNet/attention.py
@@ -54,6 +54,9 @@ class DilatedAttention(nn.Module):
         if use_rel_pos_bias:
             self.relative_bias = RelativePositionBias(num_buckets=32, max_distance=128, n_heads=num_heads)
 
+        #head offsets
+        self.head_offsets = nn.Parameer(torch.randn(num_heads, d_model))
+
     def get_mask(self, i, j):
         return torch.ones((i, j), device=device, dtype=torch.bool).triu(j - i + 2)
 
@@ -138,3 +141,126 @@ class LongNetTransformer(nn.Module):
         output = torch.sum(torch.stack(outputs), dim=0)
 
         return output
+    
+
+
+
+#second iteration
+class DilatedAttention2(nn.Module):
+    def __init__(self, d_model, num_heads, dilation_rate, segment_size, dropout=0.0, casual=False, use_xpos=False, use_rel_pos_bias=False):
+        super(DilatedAttention, self).__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+
+        self.dilation_rate = dilation_rate
+        self.segment_size = segment_size
+
+        self.attentions = nn.ModuleList([FlashMHA(embed_dim=d_model, num_heads=num_heads, device=device, dtype=dtype) for _ in range(self.dilation_rate)])
+        self.dropout = nn.Dropout(dropout)
+        self.casual = casual
+
+        self.use_xpos = use_xpos
+        self.use_rel_pos_bias = use_rel_pos_bias
+
+        if use_xpos:
+            self.xpos = XPOS(head_dim=d_model//num_heads)
+        if use_rel_pos_bias:
+            self.relative_bias = RelativePositionBias(num_buckets=32, max_distance=128, n_heads=num_heads)
+
+    def get_mask(self, i, j):
+        return torch.ones((i, j), device=device, dtype=torch.bool).triu(j - i + 2)
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.shape
+
+        if self.use_xpos:
+            x = self.xpos(x)
+
+        #collect outputs from each attention head
+        all_head_outputs = []
+        for head_idx, attention in enumerate(self.attentions):
+            offset = head_idx % self.dilation_rate
+
+            x_ = x[:, offset::self.dilation_rate, :]  # Apply offset for each head
+            x_ = x_.contiguous().view(batch_size, -1, self.segment_size, self.d_model)
+
+            attn_output, _ = attention(x_, x_, x_)
+            if self.use_rel_pos_bias:
+                attn_output += self.relative_bias(batch_size, attn_output.size(1), attn_output.size(1))
+
+            if self.casual:
+                mask = self.get_mask(attn_output.size(1), attn_output.size(1))
+                attn_output = attn_output.masked_fill(mask, float('-inf'))
+
+            attn_output = self.dropout(attn_output)
+
+            #resize back to original size
+            attn_output_resized = torch.zeros((batch_size, seq_len, self.d_model), device=device, dtype=dtype)
+            attn_output_resized[:, offset::self.dilation_rate, :] = attn_output.contiguous().view(batch_size, -1, self.d_model)
+            
+            all_head_outputs.append(attn_output_resized)
+
+        #concatenate the outputs of different heads
+        outputs_concatenated = torch.cat(all_head_outputs, dim=-1)
+
+        return outputs_concatenated
+
+
+
+
+
+# class DilatedAttention(nn.Module):
+#     def __init__(self, d_model, num_heads, dilation_rate, segment_size, dropout=0.0, casual=False, use_xpos=False, use_rel_pos_bias=False):
+#         super(DilatedAttention, self).__init__()
+#         self.d_model = d_model
+#         self.num_heads = num_heads
+
+#         self.dilation_rate = dilation_rate
+#         self.segment_size = segment_size
+
+#         self.attentions = nn.ModuleList([FlashMHA(embed_dim=d_model, num_heads=num_heads, device=device, dtype=dtype) for _ in range(self.dilation_rate)])
+#         self.dropout = nn.Dropout(dropout)
+#         self.casual = casual
+
+#         self.use_xpos = use_xpos
+#         self.use_rel_pos_bias = use_rel_pos_bias
+
+#         if use_xpos:
+#             self.xpos = XPOS(head_dim=d_model//num_heads)
+#         if use_rel_pos_bias:
+#             self.relative_bias = RelativePositionBias(num_buckets=32, max_distance=128, n_heads=num_heads)
+
+#     def get_mask(self, i, j):
+#         return torch.ones((i, j), device=device, dtype=torch.bool).triu(j - i + 2)
+
+#     def forward(self, x):
+#         batch_size, seq_len, _ = x.shape
+
+#         if self.use_xpos:
+#             x = self.xpos(x)
+
+#         x = x.view(batch_size, -1, self.segment_size, self.d_model)
+#         x = x[:, :, :: self.dilation_rate, :]
+
+#         # collect output from each attention head
+#         attn_outputs = []
+#         for attention in self.attentions:
+#             attn_output, _ = attention(x, x, x)
+#             if self.use_rel_pos_bias:
+#                 attn_output += self.relative_bias(batch_size, attn_output.size(1), attn_output.size(1))
+
+#             if self.casual:
+#                 mask = self.get_mask(attn_output.size(1), attn_output.size(1))
+#                 attn_output = attn_output.masked_fill(mask, float('-inf'))
+
+#             attn_output = self.dropout(attn_output)
+#             attn_outputs.append(attn_output)
+
+#         # apply weighted sum and softmax
+#         attn_outputs = torch.stack(attn_outputs, dim=0)
+#         attn_outputs = F.softmax(attn_outputs, dim=0)
+#         attn_outputs = torch.sum(attn_outputs, dim=0)
+
+#         attn_outputs = attn_outputs.view(batch_size, -1, self.d_model)
+
+#         return attn_outputs
