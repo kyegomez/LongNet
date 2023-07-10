a3d1d185456561290364c76145041740ca299387
Kye <kye@apacmediasolutions.com>
2023-07-10 14:03:30 -0400

    torchscale imports

--- LongNet/attention.py
+++ LongNet/attention.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from torchscale import XPOS, RelativePositionBias
+from torchscale.component.xpos_relative_position import XPOS
+from torchscale.component.relative_position_bias import RelativePositionBias
 
 from LongNet.attend import FlashMHA
 
