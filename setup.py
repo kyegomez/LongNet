168cb698c156aa46bcfd70128ffc9cfeae141560
Kye <kye@apacmediasolutions.com>
2023-07-10 15:45:17 -0400

    clean up with setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'LongNet',
   packages = find_packages(exclude=[]),
-  version = '0.2.4',
+  version = '0.2.5',
   license='MIT',
   description = 'LongNet - Pytorch',
   author = 'Kye Gomez',
