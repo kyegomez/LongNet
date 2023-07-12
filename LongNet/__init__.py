7f52ff674b7f135baae8563a2274d8019237110e
Kye <kye@apacmediasolutions.com>
2023-07-12 15:04:39 -0400

    clean up

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -1,2 +1,2 @@
-from LongNet.attention import DilatedAttention, MultiModalDilationAttention, DilatedAttention2, DistributedDilatedAttention
+from LongNet.attention import DilatedAttention
 from LongNet.model import LongNetTokenizer, LongNet, DecoderConfig, Decoder
