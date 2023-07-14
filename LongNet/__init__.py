4cdb04c1073e8ec772586003c487a5e6be52cc29
Kye <kye@apacmediasolutions.com>
2023-07-14 15:12:09 -0400

    dilated long net

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -1,4 +1,4 @@
 from LongNet.attention import DilatedAttention
-from LongNet.model import LongNetTokenizer, LongNet, DecoderConfig, Decoder
+from LongNet.model import LongNetTokenizer, LongNet, DecoderConfig, Decoder, DilatedLongNet
 
 from LongNet.iterations import DynamicDilatedAttention, DilatedAttentionOld, DilatedAttentionOP
