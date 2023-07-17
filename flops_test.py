018e2e74ae6e17db4f7114a21bce6b353ab50847
Kye <kye@apacmediasolutions.com>
2023-07-17 16:14:02 -0400

    update

--- flops_test.py
+++ flops_test.py
@@ -1,12 +1,11 @@
 import torch
-from pthflops import count_ops
+import torch.nn.functional as F
 import time
-import matplotlib.pyplot as plt
 
 from LongNet.attention import DilatedAttention
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Initialize parameters
 bsz = 32
 d_model = 512
 num_heads = 8
@@ -19,51 +18,58 @@ use_rel_pos_bias = False
 
 sequence_lengths = list(range(500, 2500, 500))
 
-time_taken = []
-tflops_per_s = []
+# Device configuration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype=torch.float32
 
+# Initialize model
 model = DilatedAttention(
-    d_model=d_model,
-    num_heads=num_heads,
-    dilation_rate=dilation_rate,
-    segment_size=segment_size,
-    dropout=dropout,
-    casual=casual,
-    use_xpos=use_xpos,
+    d_model=d_model, 
+    num_heads=num_heads, 
+    dilation_rate=dilation_rate, 
+    segment_size=segment_size, 
+    dropout=dropout, 
+    casual=casual, 
+    use_xpos=use_xpos, 
     use_rel_pos_bias=use_rel_pos_bias
 ).to(device)
 
+time_taken = []
+tflops_per_s = []
+
+# Benchmark model
 for seq_len in sequence_lengths:
-    x = torch.randn(bsz, seq_len, d_model).to(device)
+    x = torch.randn(bsz, seq_len, d_model).to(device).type(dtype)
+    torch.cuda.synchronize()
 
-    start_time = time.time()
+    start = time.time()
     output = model(x)
-    end_time = time.time()
+    torch.cuda.synchronize()
+    elapsed = time.time() - start
 
-    # Count operations on a single forward pass
-    count, _ = count_ops(model, x)
+    time_taken.append(elapsed)
+    total_flops = 4 * seq_len**2 * (d_model // num_heads) * num_heads
+    tflops_per_s.append(total_flops / elapsed / 1e12)  # Convert to TFLOPs
 
-    # Convert to TFLOPs
-    tflops = count / 1e12
+# Print benchmark results
+for seq_len, elapsed, tflops in zip(sequence_lengths, time_taken, tflops_per_s):
+    print(f"Sequence length: {seq_len}, Time elapsed: {elapsed} s, TFLOPs/s: {tflops}")
 
-    # Calculate TFLOPs/s
-    tflops_per_s.append(tflops / (end_time - start_time))
-    time_taken.append(end_time - start_time)
 
-# Plotting
-plt.figure(figsize=(10,4))
+# # Plotting
+# plt.figure(figsize=(10,4))
 
-plt.subplot(1,2,1)
-plt.plot(sequence_lengths, time_taken)
-plt.title('Time Taken vs Sequence Length')
-plt.xlabel('Sequence Length')
-plt.ylabel('Time Taken (s)')
+# plt.subplot(1,2,1)
+# plt.plot(sequence_lengths, time_taken)
+# plt.title('Time Taken vs Sequence Length')
+# plt.xlabel('Sequence Length')
+# plt.ylabel('Time Taken (s)')
 
-plt.subplot(1,2,2)
-plt.plot(sequence_lengths, tflops_per_s)
-plt.title('Performance vs Sequence Length')
-plt.xlabel('Sequence Length')
-plt.ylabel('Performance (TFLOPs/s)')
+# plt.subplot(1,2,2)
+# plt.plot(sequence_lengths, tflops_per_s)
+# plt.title('Performance vs Sequence Length')
+# plt.xlabel('Sequence Length')
+# plt.ylabel('Performance (TFLOPs/s)')
 
-plt.tight_layout()
-plt.show()
+# plt.tight_layout()
+# plt.show()
