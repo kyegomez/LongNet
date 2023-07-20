9828d156cc5e037ae1edb961fac6afe2473a1b23
Kye <kye@apacmediasolutions.com>
2023-07-20 07:51:46 -0400

    training script might work now with testing

--- LongNet/train.py
+++ LongNet/train.py
@@ -17,7 +17,6 @@ from accelerate.utils import (DummyOptim, DummyScheduler,
                               InitProcessGroupKwargs)
 from datasets import concatenate_datasets, load_dataset
 from lion_pytorch import Lion
-from torch.nn import LayerNorm
 
 from torch.nn import LayerNorm
 
@@ -25,7 +24,7 @@ from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     CheckpointImpl, apply_activation_checkpointing, checkpoint_wrapper)
 
 from torch.distributed.fsdp.wrap import (
-    transformer_auto_wrap_policy,
+    transformer_auto_wrap_policy
 )
 
 
@@ -36,11 +35,13 @@ from transformers import (AutoTokenizer, default_data_collator,
                           get_cosine_schedule_with_warmup,
                           get_linear_schedule_with_warmup, set_seed)
 
-
-
-from torchscale import Decoder
-from utils import StableAdamWUnfused
+from LongNet.utils import StableAdamWUnfused
 from LongNet.model import LongNet
+
+
+import bitsandbytes as bnb
+
+
 
 ############ SETUP CONFIG
 # import torch.distributed as dist
@@ -50,7 +51,7 @@ from LongNet.model import LongNet
 ################
 
 class CFG:
-    BATCH_SIZE: int = 3
+    BATCH_SIZE = 3
     GRADIENT_ACCUMULATE_EVERY: int = 1
     SEED: int = 42
     LEARNING_RATE: float = 3e-4
@@ -58,19 +59,20 @@ class CFG:
     SEQ_LEN: int = 8192
     NUM_CPU: int = multiprocessing.cpu_count()
     USE_DEEPSPEED: bool = True
-    USE_FSDP: bool = False
-    USE_PRETOKENIZED: bool = False
-    USE_ACTIVATION_CHECKPOINTING: bool = False
-    RESUME_FROM_CHECKPOINT: str = None
+    USE_FSDP: bool = True
+    USE_PRETOKENIZED: bool = True
+    USE_ACTIVATION_CHECKPOINTING: bool = True
+    RESUME_FROM_CHECKPOINT: str = False
     CHECKPOINTING_STEPS: int = 1000
-    OUTPUT_DIR: str = "YOUR_OUTPUT_DIR"
-    ENTITY_NAME: str = "YOUR_ENTITY_NAME" #wandb
+    OUTPUT_DIR: str = 'checkpoints/' # Folder
+    ENTITY_NAME: str = "LongNet"
 
 
 # helpers
 
 
 def print_num_params(model, accelerator: Accelerator):
+    # n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     accelerator.print(f"Number of parameters in model: {n_params}")
 
@@ -93,8 +95,7 @@ def activation_checkpointing(
     """
     if accelerator is not None:
         accelerator.print(f"Using activation checkpointing")
-    #maybe error here in decoder, use parallel transformer block
-    check_fn = lambda submodule: isinstance(submodule, Decoder)
+    check_fn = lambda submodule: isinstance(submodule, TransformerWrapper)
     non_reentrant_wrapper = partial(
         checkpoint_wrapper,
         offload_to_cpu=offload_to_cpu,
@@ -134,7 +135,7 @@ def fsdp(
         LongNet_auto_wrap_policy = partial(
             transformer_auto_wrap_policy,
             transformer_layer_cls={
-                Decoder,
+                TransformerWrapper,
             },
         )
     else:
@@ -376,6 +377,10 @@ def decoupled_optimizer(
         optimizer = StableAdamWUnfused(
             grouped_params, lr=learning_rate, betas=(beta_1, beta_2),
         )
+    elif optimizer_type=="Adam8bit":
+        optimizer = bnb.optim.Adam8bit(grouped_params, lr=learning_rate, betas=(beta_1, beta_2))
+    elif optimizer_type=="Lion8Bit":
+        optimizer = bnb.optim.Lion8bit(grouped_params, lr=learning_rate, betas=(beta_1, beta_2))
     else:
         raise ValueError(
             "Invalid optimizer_type. Expected 'lion', 'adamw', 'deepspeed' or 'stable_adamw', got: {}".format(
@@ -439,13 +444,13 @@ def build_dataloaders():
 
 #switch to falconwebdataset
 def build_pre_tokenized():
-    d0 = load_dataset("conceptofmind/c4_0-to-20_neox_with_eos_8k", split="train")
-    d1 = load_dataset("conceptofmind/c4_21-to-40_neox_with_eos_8k", split="train")
-    d2 = load_dataset("conceptofmind/c4_41-to-60_neox_with_eos_8k", split="train")
-    d3 = load_dataset("conceptofmind/c4_61-to-80_neox_with_eos_8k", split="train")
-    d4 = load_dataset("conceptofmind/c4_81-to-100_neox_with_eos_8k", split="train")
-    train_dataset = concatenate_datasets([d0, d1, d2, d3, d4])
-    return train_dataset
+    d0 = load_dataset("conceptofmind/c4_0-to-20_neox_with_eos_8k", split="train[:10]")
+    # d1 = load_dataset("conceptofmind/c4_21-to-40_neox_with_eos_8k", split="train")
+    # d2 = load_dataset("conceptofmind/c4_41-to-60_neox_with_eos_8k", split="train")
+    # d3 = load_dataset("conceptofmind/c4_61-to-80_neox_with_eos_8k", split="train")
+    # d4 = load_dataset("conceptofmind/c4_81-to-100_neox_with_eos_8k", split="train")
+    # train_dataset = concatenate_datasets([d0, d1, d2, d3, d4])
+    return d0
 
 
 
@@ -480,10 +485,7 @@ def Train():
 
     set_seed(CFG.SEED)
 
-
-    # model = LongNet.to(accelerator.device)
-    # model = AutoModelForCausalLM.from_pretrained("YOUR MODEL", load_in_4bit=True, device_map="auto").to(accelerator.device)
-    LongNet().to(accelerator.device)
+    model = LongNet().to(accelerator.device)
 
     print_num_params(model, accelerator)
 
@@ -518,7 +520,7 @@ def Train():
         weight_decay=CFG.WEIGHT_DECAY, 
         beta_1=0.90, 
         beta_2=0.95, 
-        optimizer_type='deepspeed',  
+        optimizer_type='Adam8bit',  
         use_fsdp=True,
         accelerator=accelerator
     )
@@ -644,5 +646,20 @@ def Train():
             )
 
 
-if __name__ == "__main__":
+def main():
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '9994'
+    
+    # # [CRITICAL] Pay attention to this when scaling to multiple GPUs and clusters
+    
+    # # Pay attention to this, use "accelerate config"
+
+    os.environ['RANK']       = str(0) # Number of nodes (servers)
+    os.environ['WORLD_SIZE'] = str(torch.cuda.device_count())
+
+    torch.distributed.init_process_group()
+    
     Train()
+
+if __name__ == '__main__':
+    main()
