9f682465a7976a1cd007f1f18ce5dc238b59f1dd
Kye <kye@apacmediasolutions.com>
2023-08-10 15:19:29 -0400

    multi head trial

--- LongNet/attention.py
+++ LongNet/attention.py
@@ -372,44 +372,81 @@ class DilatedAttention(nn.Module):
 
 
 
+class MultiHeadDilatedAttention(DilatedAttention):
+    """
+    **Description:**  
+    The `MultiHeadDilatedAttention` class offers a multi-head version of dilated attention, allowing the model to focus on various parts of an input sequence with different patterns.
 
+    **Parameters:**  
+    - `d_model`: Dimension of the model. This is usually a multiple of `num_heads`.
+    - `num_heads`: Number of attention heads.
+    - `dilation_rate`: The rate of dilation for the dilated attention mechanism.
+    - `segment_size`: The size of the segment for dilated attention.
 
+    Additionally, the parameters from the parent class `DilatedAttention` are inherited.
 
+    **Methods:**  
+    - `sparsify(x, sj, head_layer)`: Transforms the tensor by applying the dilated attention offset based on the given head layer. It's used internally and typically not called directly.
+    - `forward(x)`: Computes the multi-head dilated attention on the input tensor `x`.
+    """
+    def __init__(
+            self,
+            d_model,
+            num_heads,
+            dilation_rate,
+            segment_size,
+            *args,
+            **kwargs,
+    ):
+        super(MultiHeadDilatedAttention, self).__init__(d_model,
+                                                        num_heads,
+                                                        dilation_rate,
+                                                        segment_size, 
+                                                        *args,
+                                                        **kwargs)
+        
+        self.head_dim  = d_model // num_heads
 
+        #define linear layers for Q, K, V projections for each head
+        self.query_layers = nn.ModuleList([nn.Linear(d_model, self.head_dim) for _ in range(num_heads)])
+        self.key_layers = nn.ModuleList([nn.Linear(d_model, self.head_dim) for _ in range(num_heads)])
+        self.value_layers = nn.ModuleList([nn.Linear(d_model, self.head_dim) for _ in range(num_heads)])
 
+        #define linear layer for concat the multi head outputs
+        self.concat_layer = nn.Linear(num_heads * self.head_dim, d_model)
 
+    def sparsify(self, x, sj, head_layer):
+        x_proj = head_layer(x)
+        batch_size, seq_len = x_proj.shape
+        padding_len = -seq_len % self.segment_size
+        x_proj = F.pad(x_proj, (0, 0, 0, padding_len))
+        seq_len += padding_len
 
+        x_proj = x_proj.view(batch_size, -1, self.segment_size, self.head_dim)
+        x_proj = x_proj[:, :, sj::self.dilation_rate, :]
 
-
-
-class MultiHeadDilatedAttention:
-    def __init__(self, d_model, num_heads, segment_size, dilation_rate, dropout=0.0, casual=False, use_xpos=False, use_rel_pos_bias=False):
-        super().__init__()
-
-        self.d_model = d_model
-        self.num_heads = num_heads
-        self.segment_size = segment_size
-        self.dilation_rate = dilation_rate
-        self.head_dim = d_model // num_heads
-
-        assert (self.head_dim * num_heads == d_model), 'Embedding dimebsion should be divisible by number of heads'
-
-        self.dilated_attention = DilatedAttention(d_model, num_heads, dilation_rate, segment_size, dropout, casual, use_xpos, use_rel_pos_bias)
+        return x_proj
     
     def forward(self, x):
         batch_size, seq_len, _ = x.shape
 
-        #calculate the necessaary padding
-        padding_len = -seq_len % self.segment_size
-        x = F.pad(x, (0, 0, 0, padding_len))
+        all_heads_output = []
+        
+        for j in range(self.num_heads):
+            sj = j % self.dilation_rate
 
-        #init output tensor
-        outputs = torch.zeros_like(x)
+            #create Q, K, V for each head based on offset
+            Qi = self.sparsify(x, sj, self.query_layers[j])
+            Ki = self.sparsify(x, sj, self.key_layers[j])
+            Vi = self.sparsify(x, sj, self.value_layers[j])
 
-        #perform dilated attention on each head
-        outputs = self.dilated_attention(x)
+            #apply inherited atention on q, k, v
+            head_output = super(MultiHeadDilatedAttention, self).forward(Qi, Ki, Vi)
+            all_heads_output.append(head_output)
 
-        return outputs
+        #concat multi head outputs and pdd through final linear layer 
+        concat_heads = torch.cat(all_heads_output, dim=-1)
+        return self.concat_layer(concat_heads)
 
 
 
