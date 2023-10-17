917fa75d4d22e936d8067338e25a0c5d81596fa3
Kye <kye@apacmediasolutions.com>
2023-10-17 19:07:48 -0400

    clean up

--- LongNet/__init__.py
+++ LongNet/__init__.py
@@ -1,7 +1,1 @@
 from longnet.attention import ParallelWrapper, DilatedAttention
-
-# from longnet.model import LongNetTokenizer, LongNet, DecoderConfig, Decoder, DilatedLongNet
-
-# from longnet.iterations import DynamicDilatedAttention, DilatedAttentionOld, DilatedAttentionOP
-
-from longnet.model import LongNet
