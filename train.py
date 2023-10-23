2e650aa5d70c284279e6f61ed3eac16244874d76
Kye <kye@apacmediasolutions.com>
2023-10-23 12:03:43 -0400

    longnet

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ class TextSamplerDataset(Dataset):
     def __getitem__(self, index):
         rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
         full_seq = self.data[rand_start : rand_start + self.seq_len + 1].long()
-        return full_seq#.cuda()
+        return full_seq  # .cuda()
 
     def __len__(self):
         return self.data.size(0) // self.seq_len
